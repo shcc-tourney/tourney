e0ce4af1ff29553c324d48625ad71b54f91a3832
shcc-tourney <shcctourney@gmail.com>
2018-02-26 05:21:42 -0800

    Add Montserrat font

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">SHCC Tourney</h1>
+          <h1>SHCC Tourney</h1>
         </header>
       </div>
     );
