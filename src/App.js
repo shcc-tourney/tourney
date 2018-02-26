f18ddfaf196f3b11d3d9081bcfca8bdfb2591372
shcc-tourney <shcctourney@gmail.com>
2018-02-26 09:59:14 -0800

    Set up react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>SHCC Tourney</h1>
-        </header>
+          <div className="navbar-fixed">
+          <nav>
+            <div className="nav-wrapper">
+              <ul id="nav-mobile" className="left hide-on-small-only">
+                <li><a href="/odds">Live Odds</a></li>
+                <li><a href="/events">Events</a></li>
+                <li><a href="/wagers">Wagers</a></li>
+              </ul>
+              <form className="right">
+                <div className="input-field">
+                  <input id="search" type="search" required/ >
+                  <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
+                  <i className="material-icons">close</i>
+                </div>
+              </form>
+            </div>
+          </nav>
+        </div>
       </div>
     );
   }
