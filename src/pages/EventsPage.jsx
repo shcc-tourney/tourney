d12c0cf2fad934c8adc1be0d22efa0574b70b29a
shcc-tourney <shcctourney@gmail.com>
2018-03-01 04:24:42 -0800

    Begin EventsPage layout

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './EventsPage.css';
 
-const EventsPage = (props) => (
-  <h1>Events Page</h1>
-);
+class EventsPage extends Component {
+  render() {
+    return (
+      <div className='EventsPage'>
+        <div className='col m4'><h3>Event Selector</h3></div>
+        <div className='col m8'><h3>Event Detail</h3></div>
+      </div>
+    );
+  }
+}
 
-export default EventsPage;
+export default connect(
+
+)(EventsPage);
