3dc4fbc8d571ba75c759159e0e93d05b200a682a
shcc-tourney <shcctourney@gmail.com>
2018-03-04 19:33:48 -0800

    Build

--- src/components/EventSelector.jsx
+++ src/components/EventSelector.jsx
@@ -18,6 +18,39 @@ const EventSelector = (props) => {
             <a href="">This is a link</a>
           </div>
         </div>
+        <div className="card">
+          <div className="card-content">
+            <span className="card-title">Event Title</span>
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div className="card-action">
+            <a href="">This is a link</a>
+            <a href="">This is a link</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-content">
+            <span className="card-title">Event Title</span>
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div className="card-action">
+            <a href="">This is a link</a>
+            <a href="">This is a link</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-content">
+            <span className="card-title">Event Title</span>
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div className="card-action">
+            <a href="">This is a link</a>
+            <a href="">This is a link</a>
+          </div>
+        </div>
       </section>
     </div>
   );
