06f8a2124e032c5c5d619d2697e77fcaa08fb15d
shcc-tourney <shcctourney@gmail.com>
2018-03-09 11:10:04 -0800

    Update utilities.js

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,14 +1,15 @@
-const dashedRightArrow = '⇢';
-const outlineRightArrow = '⇨';
+export const dashedRightArrow = '⇢';
+export const outlineRightArrow = '⇨';
 
-
-// returns a string like 'Apr 13 - Apr 14 2018'
+// returns a string like 'Apr 13 -> Apr 14 2018'
 // or 'Fri Apr 13 - Sat Apr 14 2018'
 export function formatDateRange(start, end, includeDay = false) {
-  let s1 = start.toDateString();
-  if (!includeDay) s1 = s1.substring(4);
+  let s1 = includeDay ? start.toDateString(start) : formatDateWithoutDay(start);
   s1 = s1.substring(0, s1.length - 4);
-  let s2 = end.toDateString();
-  if (!includeDay) s2 = s2.substring(4);
+  let s2 = includeDay ? start.toDateString(end) : formatDateWithoutDay(end);
   return `${s1} ${dashedRightArrow} ${s2}`;
+}
+
+export function formatDateWithoutDay(date) {
+  return date.toDateString().substring(4);
 }
