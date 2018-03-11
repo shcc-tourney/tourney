7bceaac1148f6e3b0421a95ee05d1c04a1be5109
shcc-tourney <shcctourney@gmail.com>
2018-03-10 21:42:33 -0800

    Add data layout and more styling to TourneyCard

--- src/components/TourneyCard.jsx
+++ src/components/TourneyCard.jsx
@@ -18,7 +18,19 @@ const TourneyCard = ({tourney}) => {
               <dd>{numDays}</dd>
             </React.Fragment>
           }
+          <dt>Min Wager / Max / Increment</dt>
+          <dd>$5<span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>$500<span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>$1</dd>
+          <hr/>
+          <dt># Wagers / $ Wagered</dt>
+          <dd>5<span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>$125</dd>
+          <dt>Computed Payout&nbsp;&nbsp;/&nbsp;&nbsp;Actual</dt>
+          <dd>$112<span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>$98</dd>
+          <dt>Computed Take&nbsp;&nbsp;/&nbsp;&nbsp;Actual</dt>
+          <dd>$13<span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>$27</dd>
         </dl>
+      </div>
+      <div className="card-action">
+        <a href="">Competitors<span data-badge-caption='' className='badge'>{tourney.competitors.length}</span></a>
       </div>
     </article >
   );
--- src/components/TourneySelector.jsx
+++ src/components/TourneySelector.jsx
@@ -33,10 +33,6 @@ const TourneySelector = ({ curTourney }) => {
             <p>I am a very simple card. I am good at containing small bits of information.
             I am convenient because I require little markup to use effectively.</p>
           </div>
-          <div className="card-action">
-            <a href="">This is a link</a>
-            <a href="">This is a link</a>
-          </div>
         </div>
       </section>
     </div>
