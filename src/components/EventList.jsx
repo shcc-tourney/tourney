0b3d1c0bc849114f7fd7568d4fdd85cecac11b07
shcc-tourney <shcctourney@gmail.com>
2018-03-22 05:43:02 -0700

    Update messaging in EventList

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import './EventList.css';
 import EventCard from './EventCard';
 import NewEventModal from './NewEventModal';
+import { isPending } from '../utils/appLogicTourney';
 
 const EventList = ({tourney}) => {
   function modal() {
@@ -11,15 +12,19 @@ const EventList = ({tourney}) => {
 
   }
   let title = tourney ? ` — ${tourney.name}` : '';
+  let current = isPending(tourney);
   return (
     <div className='EventList col-section'>
       <div className='section-title'>EVENTS<span>{title}</span></div>
-      {tourney && <div className='section-control-bar'><button className='btn-small' onClick={modal}>Create Event</button></div>}
+      {tourney && current && <div className='section-control-bar'><button className='btn-small' onClick={modal}>Create Event</button></div>}
       { tourney ?
         tourney.events.length ?
           tourney.events.map(e => <EventCard event={e} key={e._id}/>)
           :
-          <h4 className='heading'>No Events for {tourney.name} Created Yet</h4>
+          current ?
+            <h4 className='heading'>No Events for {tourney.name} Created Yet</h4>
+            :
+            <h4 className='heading'>The {tourney.name} Is No Longer Current</h4>
         :
         <h4 className='heading'>No Tourney Selected</h4>
       }
--- src/utils/appLogicTourney.js
+++ src/utils/appLogicTourney.js
@@ -1,9 +1,11 @@
 import { dateDiff, todayWithoutTime } from './utilities';
 
 export function numDays(tourney) {
+  if (!tourney) return 0;
   return dateDiff(tourney.startDate, tourney.endDate, 'days') + 1;
 }
 
 export function isPending(tourney) {
+  if (!tourney) return false;
   return (dateDiff(todayWithoutTime(), tourney.endDate, 'days') >= 0);
 }
