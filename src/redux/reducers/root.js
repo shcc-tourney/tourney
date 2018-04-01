f8f62c6d03f586e60e90b8c938bb2e076f78e179
shcc-tourney <shcctourney@gmail.com>
2018-04-01 10:36:24 -0700

    Add competitors reducer to root

--- src/redux/reducers/root.js
+++ src/redux/reducers/root.js
@@ -3,10 +3,12 @@ import systemReducer from './system';
 import userReducer from './user';
 import tourneysReducer from './tourneys';
 import eventsReducer from './events';
+import competitorsReducer from './competitors';
 
 export default combineReducers({
   systemState: systemReducer,
   userState: userReducer,
   tourneysState: tourneysReducer,
   eventsState: eventsReducer,
+  competitorsState: competitorsReducer,
 });
