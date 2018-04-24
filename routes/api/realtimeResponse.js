8da52d752c90e69b44c791cb83c8873d6a7274d0
shcc-tourney <shcctourney@gmail.com>
2018-04-24 12:29:29 -0700

    Add route to realtimeResponse for creating a competitor

--- routes/api/realtimeResponse.js
+++ routes/api/realtimeResponse.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var checkAuth = require('../../config/auth').checkAuth;
 var tourneysCtrl = require('../../controllers/tourneys');
+var competitorsCtrl = require('../../controllers/competitors');
 var ioServer = require('../../realtime/io');
 var messages = require('../../realtime/socket-messages');
 
@@ -11,6 +12,12 @@ var messages = require('../../realtime/socket-messages');
 router.put('/events', checkAuth, markRealtimeResponse, tourneysCtrl.updateEvent, function(req, res) {
   var io = ioServer.get();
   io.in('auth').emit(messages.UPDATE_TOURNEY, res.locals.updatedTourney);
+  res.json({ msg: 'successful' });
+});
+
+router.post('/competitors', checkAuth, markRealtimeResponse, competitorsCtrl.create, function (req, res) {
+  var io = ioServer.get();
+  io.in('auth').emit(messages.SET_COMPETITORS, res.locals.competitors);
   res.json({ msg: 'successful' });
 });
 
